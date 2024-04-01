2dbed942baa3f8a02dcd05cfee212853d90863fc
DLimaRafael <rafaeldiaslima@protonmail.com>
2024-04-01 14:12:20 -0300

    feat: task list initial

---
+++ src/lib/Checkbox.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let isChecked = false;
+  export let handleClick = () => {};
+
+  $: background = isChecked ? "bg-zinc-400" : "none";
+</script>
+
+<button
+  class="w-6 h-6 rounded-full overflow-hidden border-2 border-zinc-400 p-0 {background}"
+  on:click={handleClick}
+/>
---
+++ src/lib/TaskList.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Checkbox from "./Checkbox.svelte";
+
+  // Task -> id, value, isDone
+  export let tasks = [];
+
+  function handleTaskEdit(value, index) {
+    tasks[index].value = value;
+  }
+
+  function handleTaskCheck(index) {
+    tasks[index].isDone = !tasks[index].isDone;
+  }
+
+  function handleTaskDelete(index) {
+    tasks.splice(index, 1);
+    tasks = tasks;
+  }
+
+  function handleTaskAdd(task = { value: "", isDone: false }) {
+    tasks = [...tasks, task];
+  }
+</script>
+
+<div class="h-screen p-8 bg-zinc-950">
+  <ul>
+    {#each tasks as task, index}
+      <li class="flex items-center gap-4">
+        <Checkbox
+          isChecked={task.isDone}
+          handleClick={() => handleTaskCheck(index)}
+        />
+        <p>{task.value}</p>
+        <button>Editar</button>
+      </li>
+    {/each}
+  </ul>
+</div>
