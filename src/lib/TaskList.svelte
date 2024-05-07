68c44532896bce0a49f30614330793b0d1040d2d
DLimaRafael <rafaeldiaslima@protonmail.com>
2024-05-07 12:39:27 -0300

    dnd prep

--- src/lib/TaskList.svelte
+++ src/lib/TaskList.svelte
@@ -1,6 +1,8 @@
 <script>
   import { dialogTask, taskData } from "../stores/tasks";
   import { generateUniqueId } from "../utils/dataManager";
+  import { dndzone } from "svelte-dnd-action";
+  import { flip } from "svelte/animate";
   import {
     createTask,
     deleteTask,
@@ -16,6 +18,8 @@
   import TaskGroup from "./TaskGroup.svelte";
 
   // Task -> id, value, isDone, children, isCollapsed
+
+  const flipDurationMs = 100;
 
   $: tasks = filterData($taskData);
   $: parentTasks = tasks.filter((task) => {
@@ -42,22 +46,40 @@
   function handleSearch() {
     tasks = filterData($taskData);
   }
+
+  function handleConsider(e) {
+    console.log(tasks !== e.detail.items);
+    tasks = e.detail.items;
+  }
+
+  function handleFinalize(e) {
+    tasks = e.detail.items;
+    console.log(e.detail.items);
+    //
+  }
 </script>
 
 <div class="list-container m-auto h-full flex flex-col max-w-screen-md">
   <MainInput onAdd={handleAddTask} onSearch={handleSearch} />
   <div class="h-full overflow-y-auto mt-4 pr-3">
-    <ul class="flex flex-col flex-1 gap-1">
+    <ul
+      class="h-full"
+      use:dndzone={{ items: tasks, flipDurationMs, dropTargetStyle: {} }}
+      on:consider={handleConsider}
+      on:finalize={handleFinalize}
+    >
       {#each parentTasks as task (task.id)}
-        <TaskGroup
-          {task}
-          {getTaskChildren}
-          {handleAddTask}
-          {getTaskProgress}
-          {handleDialog}
-          {saveTask}
-          {deleteTask}
-        />
+        <div animate:flip={{ duration: flipDurationMs }}>
+          <TaskGroup
+            {task}
+            {getTaskChildren}
+            {handleAddTask}
+            {getTaskProgress}
+            {handleDialog}
+            {saveTask}
+            {deleteTask}
+          />
+        </div>
       {/each}
     </ul>
   </div>
